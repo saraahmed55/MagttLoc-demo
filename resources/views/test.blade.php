b5699b37a674f1b3e99eace47b0c583aa27eef92
Sara Elmogy <62851641+saraahmed55@users.noreply.github.com>
2022-10-29 08:57:02 +0200

    trace

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -10,6 +10,7 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <script type="text/javascript" src="js/app.js"></script>
     <style>
+
         #canvas{
             background-image: url("img/layout_emp.png");
             background-size: 100% 100%;
@@ -27,7 +28,9 @@
 <body>
     <div>
         <div class="row position-relative">
+            
             <div class="col-11" ><canvas id="canvas"></canvas></div>
+
             <div class="col-1 position-absolute top-0 end-0" id="box">
                 <div class="form-check form-switch">
                     <input class="form-check-input" type="checkbox" id="points">
@@ -47,6 +50,18 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <script>
         $(document).ready(function(){
+            // fetchDataTraceAnimation();
+
+// vertices.push({x:0,y:0});
+// vertices.push({x:300,y:100});
+// vertices.push({x:80,y:200});
+// vertices.push({x:10,y:100});
+// vertices.push({x:0,y:0});
+// var points=calcWaypoints(vertices);
+// console.log(points)
+
+
+var t=1;
 
             var points=document.getElementById("points");
             var trace=document.getElementById("trace");
@@ -64,10 +79,31 @@
             document.body.appendChild(canvas);
             ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height);
 
+            var verticesX=fetchDataTraceAnimationX();
+
+            // console.log(verticesX[0])
+
+
+
+            // animate();
+
+            function animate(){
+                if(t<points.length-1){ requestAnimationFrame(animate); }
+                // draw a line segment from the last waypoint
+                // to the current waypoint
+                ctx.beginPath();
+                ctx.moveTo(points[t-1].x,points[t-1].y);
+                ctx.lineTo(points[t].x,points[t].y);
+                ctx.stroke();
+                // increment "t" to get the next waypoint
+                t++;
+            }
+
             function Start(){
-                if(points.checked==true){
+
+                if(points.checked==true && trace.checked==false){
                     fetchDataPoints();
-                }else if(trace.checked==true)
+                }else if(trace.checked==true && points.checked==false)
                 {
                     fetchDataTrace();
                 }else{
@@ -77,7 +113,6 @@
                     }, 2000);
                 }
             }
-
 
             function drawPoint(context, x, y, color, size) {
 
@@ -123,10 +158,8 @@
                                 var Predicted_X_px=(96 * Predicted_X_cm)/2.54;
                                 var Predicted_Y_px=(96 * Predicted_Y_cm)/2.54;
 
-                                setTimeout(function() {
-                                    drawPoint(ctx, Actual_X_px,Actual_Y_px, 'blue', 4);
-                                    drawPoint(ctx, Predicted_X_px,Predicted_Y_px, 'red', 4);
-                                }, 2000);
+                                drawPoint(ctx, Actual_X_px,Actual_Y_px, 'blue', 4);
+                                drawPoint(ctx, Predicted_X_px,Predicted_Y_px, 'green', 4);
 
                                 AX=Actual_X_px;
                                 AY=Actual_Y_px;
@@ -174,14 +207,14 @@
                             }else{
 
                                 ctx.beginPath();
-                                ctx.lineWidth = 5;
+                                ctx.lineWidth = 3;
                                 ctx.strokeStyle = 'blue';
                                 ctx.moveTo(AX,AY);
                                 ctx.lineTo(Actual_X_px,Actual_Y_px);
                                 ctx.stroke();
                                 ctx.beginPath();
-                                ctx.lineWidth = 7;
-                                ctx.strokeStyle = 'red';
+                                ctx.lineWidth = 3;
+                                ctx.strokeStyle = 'green';
                                 ctx.moveTo(PX,PY);
                                 ctx.lineTo(Predicted_X_px,Predicted_Y_px);
                                 ctx.stroke();
@@ -189,8 +222,8 @@
                             }
 
                             setTimeout(function() {
-                                drawPoint(ctx, Actual_X_px,Actual_Y_px, 'blue', 4);
-                                drawPoint(ctx, Predicted_X_px,Predicted_Y_px, 'red', 4);
+                                drawPoint(ctx, Actual_X_px,Actual_Y_px, 'blue', 2);
+                                drawPoint(ctx, Predicted_X_px,Predicted_Y_px, 'green', 2);
                             }, 1000);
 
                             AX=Actual_X_px;
@@ -202,6 +235,109 @@
                 });
             }
 
+            function fetchDataTraceAnimationX(){
+                var arrActualX=[];
+                $.ajax({
+                    type:"GET",
+                    url:"test-points",
+                    dataType:"json",
+                    success:function(response){
+                        var count=0;
+                        var i=0;
+                        $.each(response.data,function(key,item){
+
+                            ++count;
+                            var Actual_X_cm=parseFloat(item.actual_x_distance)/100;
+
+                            var Actual_X_px=(96 * Actual_X_cm)/2.54;
+
+                            arrActualX[i]=Actual_X_px;
+                            i++;
+
+                        });
+                    }
+
+                });
+
+                console.log(arrActualX)
+                return arrActualX;
+            }
+
+            function fetchDataTraceAnimationY(){
+                var arrActualY=[];
+                $.ajax({
+                    type:"GET",
+                    url:"test-points",
+                    dataType:"json",
+                    success:function(response){
+                        var count=0;
+                        $.each(response.data,function(key,item){
+
+                            ++count;
+                            var Actual_Y_cm=parseFloat(item.actual_y_distance)/100;
+
+                            var Actual_Y_px=(96 * Actual_Y_cm)/2.54;
+
+                            arrActualY.push(Actual_Y_px);
+
+                            if( count == 1){
+                                AX=Actual_Y_px;
+
+                            }else{
+
+                                // ctx.beginPath();
+                                // ctx.lineWidth = 3;
+                                // ctx.strokeStyle = 'blue';
+                                // ctx.moveTo(AX,AY);
+                                // ctx.lineTo(Actual_X_px,Actual_Y_px);
+                                // ctx.stroke();
+                                // ctx.beginPath();
+                                // ctx.lineWidth = 3;
+                                // ctx.strokeStyle = 'green';
+                                // ctx.moveTo(PX,PY);
+                                // ctx.lineTo(Predicted_X_px,Predicted_Y_px);
+                                // ctx.stroke();
+
+                            }
+                            AX=Actual_Y_px;
+                        });
+                    }
+
+                });
+
+                return arrActualY;
+            }
+
+            function calcWaypointsX(vertices){
+                var waypoints=[];
+                for(var i=1;i<vertices.length;i++){
+                    var pt0=vertices[i-1];
+                    var pt1=vertices[i];
+                    var dx=pt1.x-pt0.x;
+                    var dy=pt1.y-pt0.y;
+                    for(var j=0;j<100;j++){
+                        var x=pt0.x+dx*j/100;
+                        var y=pt0.y+dy*j/100;
+                        waypoints.push(x);
+                    }
+                }
+                return(waypoints);
+            }
+            function calcWaypointsY(vertices){
+                var waypoints=[];
+                for(var i=1;i<vertices.length;i++){
+                    var pt0=vertices[i-1];
+                    var pt1=vertices[i];
+                    var dx=pt1.x-pt0.x;
+                    var dy=pt1.y-pt0.y;
+                    for(var j=0;j<100;j++){
+                        var x=pt0.x+dx*j/100;
+                        var y=pt0.y+dy*j/100;
+                        waypoints.push(y);
+                    }
+                }
+                return(waypoints);
+            }
 
         });
     </script>
